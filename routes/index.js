89c87b7cf3d93a7abb4c2bfc88aab0696580e6ea
cyqg <cyqg@fresnou.org>
2022-02-01 08:37:51 -0800

    progress commit

--- routes/index.js
+++ routes/index.js
@@ -23,4 +23,7 @@ router.get('/students/add', studentController.renderAddForm);
 router.post('/students/add', studentController.addStudent);
 router.get('/students/delete/:id', studentController.deleteStudent);
 
+router.post('/students/:studentId/enroll/', studentController.enrollStudent);
+
+
 module.exports = router;
